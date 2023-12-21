114439229d089e1849846c13adbbea03c5fb6e8b
juan <juan.erices04@inacapmail.cl>
2023-12-21 15:32:42 -0300

    linea en setting heronku

--- toqr/settings.py
+++ toqr/settings.py
@@ -122,6 +122,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Heroku
+# Heroku - django on heroku
 import django_on_heroku
 django_on_heroku.settings(locals())
